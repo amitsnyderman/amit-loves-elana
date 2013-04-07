617840330d1d488cff20438877e0eac7275dbee3
Amit Snyderman <amit@amitsnyderman.com>
2013-04-07 12:20:08 -0400

    Add Susy compass dependency

--- config.rb
+++ config.rb
@@ -1,7 +1,9 @@
+require "susy"
+
 http_path = "/"
 css_dir = "assets/stylesheets"
 sass_dir = "assets/sass"
 images_dir = "assets/images"
-javascripts_dir = "assets/javascripts"
+javascripts_dir = "assets/scripts"
 output_style = :compressed
 line_comments = false
